adb005d5b8f1df3675ba152416d0e3e73e7f0674
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-23 23:14:34 -0400

    highlighted navs with scroll event

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const welcomeSection = document.querySelector('.random-background');
 const profileImage = document.querySelector('#profile-image');
 
-// ==============================
+// NAV LINKS
+const welcomeLink = document.querySelector('.welcome-link');
+const recommendationsLink = document.querySelector('.recommendations-link');
+const educationLink = document.querySelector('.education-link');
+const certificationLink = document.querySelector('.certification-link');
+const experienceLink = document.querySelector('.experience-link');
+const projectsLink = document.querySelector('.projects-link');
+
+// ===============================================
+// HIGHLIGHT NAVIGATION WITH CORRESPONDING PAGE
+// ===============================================
+
+document.addEventListener('scroll', () => {
+  let vertical = window.scrollY;
+  console.log(vertical);
+
+  // HOME
+  vertical < 700
+    ? welcomeLink.classList.add('current')
+    : welcomeLink.classList.remove('current');
+
+  // RECOMMENDATIONS
+  if (vertical > 700 && vertical < 1240) {
+    recommendationsLink.classList.add('current');
+  } else if (vertical < 700 || vertical > 1240) {
+    recommendationsLink.classList.remove('current');
+  }
+
+  // EDUCATION
+  if (vertical > 1240 && vertical < 2120) {
+    educationLink.classList.add('current');
+  } else if (vertical < 1240 || vertical > 2140) {
+    educationLink.classList.remove('current');
+  }
+
+  // CERTIFICATION
+  if (vertical > 2140 && vertical < 3080) {
+    certificationLink.classList.add('current');
+  } else if (vertical < 2140 || vertical > 3080) {
+    certificationLink.classList.remove('current');
+  }
+
+  // EXPERIENCE
+  if (vertical > 3080 && vertical < 4950) {
+    experienceLink.classList.add('current');
+  } else if (vertical < 3080 || vertical > 4950) {
+    experienceLink.classList.remove('current');
+  }
+
+  // PROJECTS
+  vertical > 4950
+    ? projectsLink.classList.add('current')
+    : projectsLink.classList.remove('current');
+});
+
+// ===============================================
 // RANDOMIZING BACKGROUND
-// ==============================
+// ===============================================
 
 // RANDOM BACKGROUND ON LOAD
 changeBackground();
