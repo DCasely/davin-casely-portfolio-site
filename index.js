e8ce41ae86196ba354426e8d824ec4dab1ee1f38
DCASELY <24211985+DCasely@users.noreply.github.com>
2020-04-23 20:10:23 -0400

    added Randomized Background on Profile Image Click

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-// LIST OF BACKGROUNDS IMAGES TO ITERATE THROUGH
-let backgroundImages = ['./images/beachverticalview.jpeg'];
+const welcomeSection = document.querySelector('.random-background');
+const profileImage = document.querySelector('#profile-image');
+
+profileImage.addEventListener('click', function () {
+  changeBackground();
+});
+
+// RANDOMIZE BACKGROUND
+function changeBackground() {
+  // LIST OF BACKGROUNDS IMAGES TO ITERATE THROUGH
+  let backgroundImages = [
+    './images/beachverticalview.jpeg',
+    './images/miami.jpg',
+    './images/codebackground.jpeg',
+    './images/heavycode.jpeg',
+    './images/mountains.jpeg',
+  ];
+
+  let random = Math.floor(Math.random() * backgroundImages.length);
+
+  let randomizedBackground = backgroundImages[random];
+
+  welcomeSection.style.background = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${randomizedBackground})`;
+  welcomeSection.style.backgroundSize = 'cover';
+}
